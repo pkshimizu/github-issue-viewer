a5c89f26fcb79dca62ec3d6668efdc54dd6b948d
shimizu kenji <shimizu.kenji@noncore.net>
2022-09-11 21:36:48 +0900

    feat: トップページのデザインを追加

---
+++ src/features/issues/index.ts
@@ -0,0 +1,3 @@
+export * from "./routes/IssueByLabelTable"
+export * from "./routes/IssueByMilestoneTable"
+export * from "./routes/IssueByProjectTable"
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,18 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import {ChakraProvider, theme} from "@chakra-ui/react";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider theme={theme}>
-      <Component {...pageProps} />
-    </ChakraProvider>
+    <>
+      <Head>
+        <title>GitHub Issue Viewer</title>
+      </Head>
+      <ChakraProvider theme={theme}>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </>
   )
 }
 
