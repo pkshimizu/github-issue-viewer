dc3c66a6bf3fc51870bed213097d7dc08dbe69dd
shimizu kenji <shimizu.kenji@noncore.net>
2022-09-25 10:47:57 +0900

    feat: タイトル設定

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -9,9 +9,7 @@ export default class Document extends NextDocument {
   render() {
     return (
       <Html lang='en'>
-        <Head>
-          <title>GitHub Issue Viewer</title>
-        </Head>
+        <Head />
         <body>
           <ColorModeScript initialColorMode={theme.config.initialColorMode} />
           <Main />
